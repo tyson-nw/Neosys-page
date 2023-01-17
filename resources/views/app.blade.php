5290fe4912026c784b4b0545b03109db341580ac
Tyson VanOverhill <tyson.vanover@gmail.com>
2023-01-17 05:09:10 +0000

    asdjhsad

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,10 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
         @inertia
-        
+
     </body>
 </html>
