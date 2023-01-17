5798ed0a59703e5fd46716e2885a1e50251edb8b
Tyson VanOverhill <tyson.vanover@gmail.com>
2023-01-17 05:09:49 +0000

    sdakj

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,31 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        @routes
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-
-    </body>
+</head>
+<body>
+    <header>
+        <div class='banner'><a href='/'>Neosys</a></div>
+        <nav>
+            <a href='/sources'>Sources</a>
+            | <a href='/pages'>Pages</a>
+            |
+            @guest
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a> | >            @endguest
+            @auth
+                Profile
+                <form method="POST" action="/logout"> @csrf <button type='submit'>Logout</button></form>
+            @endauth
+        </nav>
+    </header>
+    <main>
+        @yield('main')
+    </main>
+    <footer>
+        @if(empty($license))
+            Copyright Double Crescent
+        @else
+            {{ $license }}
+        @endif
+    </footer>
+</body>
 </html>
